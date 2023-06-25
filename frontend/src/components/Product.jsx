edfe67f1c40b30bdb2357a86e0e1fa61aedd683c
ameya-6964 <belvalkar7@gmail.com>
2023-06-25 20:42:12 +0530

    Implemented Review and Changed Price Styles

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
@@ -9,11 +11,17 @@ const Product = ({ product }) => {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
+          <Card.Text as="div">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} Reviews `}
+            />
+          </Card.Text>
         </Link>
-        <Card.Text>INR {product.price}</Card.Text>
+        <Card.Text as="h4"> ₹ {product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
