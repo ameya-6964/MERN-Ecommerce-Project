c4532e107dbe36341a7e45af672ff831dc386cf7
ameya-6964 <belvalkar7@gmail.com>
2023-06-26 14:56:15 +0530

    Imported Products From Database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/product.js";
+
+import productRoutes from "./routes/productRoutes.js";
 import connectDb from "./config/db.js";
 
 dotenv.config();
@@ -13,14 +14,7 @@ app.get("/", (req, res) => {
   res.send("API is Running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/product/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server Running on ${port}`);
